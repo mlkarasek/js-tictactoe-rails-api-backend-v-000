123433a8f68bc7114fb2d108d537231e46b78d98
Monica  Karasek <mlkarasek@gmail.com>
2018-10-16 20:12:47 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,35 +1,40 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
-  before_action :set_game, only: [:show, :update]
-
-    def index
-      @games = Game.all
-      render json: @games
-    end
-
-    def create
-      @game = Game.create(game_params)
-      render json: @game, status: 2017
-    end
-
-    def show
-      @game = Game.find(params[:id])
-      respond_to do |format|
-        format.html { render :show }
-        format.json { render json: @game }
-      end
-
-      def update
-        @game.update(game_params)
-        render json: @game, status: 204
-    end
-
-private
-
-    def game_params
-      params.require(:game).permit(:state)
-    end
-
-    def set_game
-      @game = Game.find(params[:id])
+  def index
+    @games = Game.all
+    render json: @games
   end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new
+    @game.state = params[:state]
+    @game.save
+
+    render json: @game
+
+  end
+
+  def show
+    @game = Game.find_by(params[:id])
+    render json: @game
+  end
+
+  def update
+    @game = Game.find_by(params[:id])
+    @game.state = params[:state]
+    @game.save
+    render json: @game
+
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:state)
+  end
+
+end
