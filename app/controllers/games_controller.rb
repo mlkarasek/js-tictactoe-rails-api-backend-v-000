e88ce741a0f68a1157b9fd9c06f2648602c8126b
Monica  Karasek <mlkarasek@gmail.com>
2018-10-16 20:00:33 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,35 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+    def index 
+      @games = Game.all
+      render json: @games
+    end 
+
+    def new 
+      @game = Game.new
+    end 
+
+    def create 
+      @game = Game.create(game_params)
+      render json: @game, status: 2017
+    end 
+
+    def show 
+      @game = Game.find(params[:id])
+      respond_to do |format|
+        format.html { render :show }
+        format.json { render json: @game }
+      end 
+
+      def update 
+        @game = Game.find_by(params[:id])
+        render json: @game 
+    end 
+
+private 
+
+    def game_params
+      params.require(:game).permit()
+    end 
 
 end
