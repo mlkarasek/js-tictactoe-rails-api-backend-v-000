7e99bc5c1d8fbe6b71fab369799ddb963cf7a9ff
Monica  Karasek <mlkarasek@gmail.com>
2018-10-16 20:12:22 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,7 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
   before_action :set_game, only: [:show, :update]
+  
     def index
       @games = Game.all
       render json: @games
@@ -32,4 +33,4 @@ private
     def set_game
       @game = Game.find(params[:id])
   end
-end
+
